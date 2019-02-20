f322fcc92f43c17a4dc1781aad27b60415500c0a
Dominik K <dominikoso@users.noreply.github.com>
2019-02-20 13:38:40 +0100

    Reinitialization of Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# bots-of-war
-Java-based Bot, made for NCDC Bots-Of-War 2019
---
+++ src/main/java/me/dominikoso/bow/BowApplication.java
@@ -0,0 +1,13 @@
+package me.dominikoso.bow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowApplication.class, args);
+	}
+
+}
